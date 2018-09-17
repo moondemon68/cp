755b341680860b516fc99838e7dec2c5277913ae
Morgen Sudyanto <moondemon68@gmail.com>
2018-09-18 00:30:54 +0700

    Template cp

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define MOD 1000000007
+
+using namespace std;
+
+int main () {
+
+    return 0;
+}
