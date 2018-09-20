b6338e336a94a1e46a5f99c161b1bddbc6a15327
Morgen Sudyanto <moondemon68@gmail.com>
2018-09-21 00:38:53 +0700

    hore

--- Kontes2/makara.cpp
+++ Kontes2/makara.cpp
@@ -16,15 +16,31 @@ int main () {
     memset (c,0,sizeof(c));
     stack<pii> s;
     for (int i=1;i<=n;i++) cin >> a[i];
-    for (int i=1;i<=n;i++) {
-      if (s.empty()) s.push(make_pair(a[i],i));
+    a[0]=INT_MAX;
+    a[n+1]=INT_MAX;
+    for (int i=0;i<=n;i++) {
+      if (s.empty()) s.push(mp(a[i],i));
       else {
-        while (!s.top().fi<a[i]) {
+        while (s.top().fi<a[i]) {
+          //cout << s.top().fi << " " << s.top().se << endl;
           s.pop();
         }
-        b[i]+=i-s.top().second;
+        b[i]+=i-s.top().se-1;
+        s.push(mp(a[i],i));
+      }
+    }
+    for (int i=n+1;i>=1;i--) {
+      if (s.empty()) s.push(mp(a[i],i));
+      else {
+        while (s.top().fi<a[i]) {
+          //cout << s.top().fi << " " << s.top().se << endl;
+          s.pop();
+        }
+        b[i]+=s.top().se-i-1;
+        s.push(mp(a[i],i));
       }
     }
     for (int i=1;i<=n;i++) cout << b[i] << " ";
+    cout << endl;
     return 0;
 }
