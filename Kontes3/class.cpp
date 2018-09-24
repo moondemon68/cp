037fd52b663edb7697363985f816d2748d26ff36
Morgen Sudyanto <moondemon68@gmail.com>
2018-09-24 23:16:03 +0700

    wa

--- Kontes3/class.cpp
+++ Kontes3/class.cpp
@@ -9,33 +9,59 @@
 
 using namespace std;
 
+LL poww(LL a, LL n, LL m) {
+  LL res=1;
+  a%=m;
+  while (n>0) {
+    if (n%2==1) res=(res*a)%m;
+    a=(a*a)%m;
+    n/=2;
+  }
+  return res;
+}
+
+bool test(LL d, LL n) {
+  LL a=2+rand()%(n-4);
+  LL x=poww(a,d,n);
+  if (x==1||x==n-1) return 1;
+  while (d!=n-1) {
+    x=(x*x)%n;
+    d*=2;
+    if (x==1) return 1;
+    if (x==-1) return 0;
+  }
+  return 0;
+}
+
+bool prima(LL n, LL k) {
+  if (n==1||n==4) return 0;
+  if (n==2||n==3) return 1;
+  LL d=n-1;
+  while (d%2==0) {
+    d/=2;
+  }
+  for (int i=1;i<=k;i++) {
+    if (!test(d,n)) return 0;
+  }
+  return 1;
+}
 int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    srand(time(NULL));
     int tc;
-    scanf("%d",&tc);
-    map<int,int> m;
+    cin >> tc;
     for (int t=1;t<=tc;t++) {
-      int k;
-      scanf("%d",&k);
-      bool y=0;
-      if (m[k]!=0) {
-        printf("%d\n",m[k]);
-        y=1;
-      }
-      for (int i=k;i>=0;i--) {
-        if (y) break;
-        bool p=1;
-        for (int j=2;j*j<=i;j++) {
-          if (i%j==0) {
-            p=0;
-            break;
-          }
-        }
-        if (p) {
-          m[k]=k-i;
-          printf("%d\n",m[k]);
+      LL x;
+      cin >> x;
+      LL ans=0;
+      for (LL i=x;i>=1;i--) {
+        if (prima(i,8)) {
+          ans=i;
           break;
         }
       }
+      cout << x-ans << endl;
     }
     return 0;
 }
