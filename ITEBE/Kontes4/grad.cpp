d3c55782547079eee6c5048a361465a0c4ccf317
Morgen Sudyanto <moondemon68@gmail.com>
2018-09-29 19:36:58 +0700

    ac 4

--- ITEBE/Kontes4/grad.cpp
+++ ITEBE/Kontes4/grad.cpp
@@ -9,12 +9,6 @@
 
 using namespace std;
 
-int cut(int a[],int l, int r) {
-  for (int i=l;i<=r;i++) {
-
-  }
-}
-
 int main () {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -22,11 +16,42 @@ int main () {
     int n;
     cin >> n;
     int a[5005];
-    for (int i=1;i<=n;i++) cin >> a[i];
-    int mini=INT_MAX;
-    for (int i=1;i<=n;i++) mini=min(mini,a[i]);
-    for (int i=1;i<=n;i++) a[i]-=mini;
-    for (int i=1;i<=n;i++) cout << a[i];
-    int ans=cut(a,l,r);
+    int cnt=0;
+    for (int i=1;i<=n;i++) {
+      cin >> a[i];
+      if (a[i]>0) cnt++;
+    }
+    a[n+1]=0;
+    bool y=1;
+    int ans=0;
+    while (1>0) {
+      if (!y) break;
+      y=0;
+      int s=1,e=1;
+      for (int i=s;i<=n;i++) {
+        if (a[i]==0) s++;
+        if (a[i+1]==0&&a[i]>0) {
+          e=i;
+          int mini=INT_MAX;
+          for (int j=s;j<=e;j++) mini=min(mini,a[j]);
+          if (e-s+1<mini) {
+            for (int j=s;j<=e;j++) a[j]=0;
+            ans+=e-s+1;
+          } else {
+            for (int j=s;j<=e;j++) a[j]-=mini;
+            ans+=mini;
+          }
+          s=e+1;
+          i=s;
+          if (s>n) break;
+        }
+      }
+      for (int i=1;i<=n;i++) {
+        if (a[i]>0) y=1;
+        //cout << a[i] << " ";
+      }
+      //cout << endl;
+    }
+    cout << ans << endl;
     return 0;
 }
