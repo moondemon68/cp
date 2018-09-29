b7047eaa8c79f1763d34ff0886a3b28336b77ec0
Morgen Sudyanto <moondemon68@gmail.com>
2018-09-29 12:33:14 +0700

    :)

--- ITEBE/Kontes4/formation.cpp
+++ ITEBE/Kontes4/formation.cpp
@@ -13,6 +13,15 @@ int main () {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    LL n,m;
+    cin >> n >> m;
+    LL g=0,ans=0;
+    for (int i=1;i*i<=min(n,m);i++) {
+      if (n%i==0&&m%i==0) {
+        g=i;
+        ans=max(ans,2*g+n/g+m/g);
+      }
+    }
+    cout << ans << endl;
     return 0;
 }
