3c2a1740fc0d846e2e71466e1e98de75f4f13150
moondemon68 <31804653+moondemon68@users.noreply.github.com>
2018-11-06 23:03:18 +0700

    aa

--- ITEBE/Kontes8/E.cpp
+++ ITEBE/Kontes8/E.cpp
@@ -10,51 +10,49 @@
 using namespace std;
 
 string s[35];
-map<string,int> id;
 
 int main () {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     int n;
-    cin >> n;
-    for (int i=1;i<=n;i++) cin >> s[i];
-    for (int i=1;i<=n;i++) id[s[i]]=i;
-    int m;
-    cin >> m;
-    vector<pair<double,int> > adj[35];
-    for (int i=1;i<=m;i++) {
-      string u,v;
-      double c;
-      cin >> u >> c >> v;
-      adj[id[u]].pb(mp(log(c),id[v]));
-    }
-    for (int i=1;i<=n;i++) {
-      for (int j=0;j<adj[i].size();j++) {
-        cout << adj[i][j].fi << endl;
+    int tc=1;
+    while (1>0) {
+      cin >> n;
+      if (n==0) return 0;
+      for (int i=1;i<=n;i++) cin >> s[i];
+      map<string,int> id;
+      for (int i=1;i<=n;i++) id[s[i]]=i;
+      int m;
+      cin >> m;
+      vector<pair<pii,double> > edge;
+      for (int i=1;i<=m;i++) {
+        string u,v;
+        double c;
+        cin >> u >> c >> v;
+        edge.pb(mp(mp(id[u],id[v]),-1*log(c)));
       }
-    }
-    double dist[35];
-    memset (dist,INT_MAX,sizeof(dist));
-    bool y=1;
-    dist[1]=0;
-    for (int i=1;i<n;i++) {
-      for (int j=0;j<adj[i].size();j++) {
-        if (dist[i]+adj[i][j].fi<dist[adj[i][j].se]) {
-          //cout << dist[adj[i][j].se] << endl;
-          if (dist[adj[i][j].se]<0) {
-            y=0;
-            break;
+      double dist[35];
+      memset (dist,123456789,sizeof(dist));
+      bool y=1;
+      dist[1]=0;
+      for (int i=1;i<n;i++) {
+        for (int j=0;j<m;j++) {
+          if (dist[edge[j].fi.se]>dist[edge[j].fi.fi]+edge[j].se) {
+            dist[edge[j].fi.se]=dist[edge[j].fi.fi]+edge[j].se;
           }
-          dist[adj[i][j].se]=dist[i]+adj[i][j].fi;
         }
       }
-      if (!y) break;
+      for (int i=0;i<m;i++) {
+        //cout << dist[edge[i].fi.se]-dist[edge[i].fi.fi]-edge[i].se << endl;
+        if (dist[edge[i].fi.se]>dist[edge[i].fi.fi]+edge[i].se) {
+          y=0;
+        }
+      }
+      cout << "Case " << tc << ": ";
+      if (!y) cout << "Yes" << endl;
+      else cout << "No" << endl;
+      tc++;
     }
-    for (int i=1;i<=n;i++) {
-      cout << dist[i] << " ";
-    }
-    if (y) cout << "YES" << endl;
-    else cout << "NO" << endl;
     return 0;
 }
